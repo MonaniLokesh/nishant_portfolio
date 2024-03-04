21f5e28fc0db2e2b354c128fc392b2089692b59a
Lokesh monani <92055680+MonaniLokesh@users.noreply.github.com>
2024-03-04 21:07:10 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -36,7 +36,11 @@ export const skills = [
         image:
           "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
       },
-
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
     ],
   },
   {
